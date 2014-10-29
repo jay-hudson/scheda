318b2514c681d4c86b604e30bdcf2c479a744424
jay-hudson <jkh30003@yahoo.com>
2014-10-29 05:36:48 +0000

    ddd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,8 +23,8 @@ Rails.application.configure do
   config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
-  git config.assets.js_compressor = :uglifier
-  config.assets.css_compressor = :sass
+  config.assets.js_compressor = :uglifier
+  # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
@@ -34,9 +34,6 @@ Rails.application.configure do
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
-  
-  #config.assets.compress = true
-  #config.assets.js_compressor = Uglifier.new(:mangle => false).compile(source)
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
